9d2baa9bde9446b6879fb62936081a3e62cf85dd
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2022-03-05 02:07:15 +0600

    Complete Pin-matche

---
+++ script.js
@@ -0,0 +1,50 @@
+// Get pin 
+function getPin() {
+
+    const randomNumber = Math.random() * 10000;
+    const pin = (randomNumber + '').split('.')[0];
+    if (pin.length === 4) {
+        return pin;
+    } else {
+        return getPin();
+    }
+}
+
+// Random Number/ Pin generate
+function generate() {
+    const randomInput = document.getElementById('random-value');
+    randomInput.value = getPin();
+}
+
+//Handle button event
+const btnContainer = document.getElementById('digit-count');
+btnContainer.addEventListener('click', function (event) {
+    const digit = event.target.innerText;
+    if (isNaN(digit)) {
+        const typedInput = document.getElementById('typed-input');
+        typedInput.value = '';
+    } else {
+        const typedInput = document.getElementById('typed-input');
+        typedInput.value = typedInput.value + digit;
+    }
+})
+
+// verify pin
+function verifyPin() {
+    const pin = document.getElementById('random-value');
+    const typedPin = document.getElementById('typed-input');
+
+    if (pin === typedPin) {
+        displayMatchedResul('block', 'none')
+    } else {
+        displayMatchedResul('none', 'block')
+    }
+}
+
+function displayMatchedResul(currectStatus, incurrectStatus) {
+    const currect = document.getElementById('correct-password');
+    currect.style.display = currectStatus;
+
+    const incurrect = document.getElementById('incorrect-password');
+    incurrect.style.display = incurrectStatus;
+}
